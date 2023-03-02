f63015404b8447f7a1ff2daaac71b5a987a57ce3
mbhargava4 <118226828+mbhargava4@users.noreply.github.com>
2023-03-02 06:31:52 -0600

    Problem 1 outline is done

---
+++ A05/matmulCompare.py
@@ -0,0 +1,96 @@
+import pyMatmul
+import numpy as np 
+import sys
+import argparse
+import random
+from time import perf_counter
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-P', '--PLOTLINE', type=float, default =1)
+
+args = parser.parse_args()
+if(args.PLOTLINE==0):
+    plotbool = False
+else:
+    plotbool = True
+
+
+#function to generate random matrix of nxn size
+def generateRandomMatrix(n):
+    C=[list(0 for i in range(n)) for j in range(n)]
+    for i in range(n):
+        for j in range(n):
+            C[i][j] = random.randint(-1,1) #numbers in this matrix are between -1 and 1
+
+    return C
+
+k = [5,6,7]
+size = list(0 for i in range(4))
+t = list(0.0 for i in range(4))
+t_1 = list(0.0 for i in range(4))
+
+for i in k:
+   
+    n=2**(i)
+    size[i-5] = n
+    A_list = generateRandomMatrix(n)
+    B_list = generateRandomMatrix(n)
+    
+    A=np.zeros([n,n])
+    B=np.zeros([n,n])
+    for a in range(n):
+        for b in range(n):
+            A[a][b]=A_list[a][b]
+            B[a][b]=B_list[a][b]
+    
+    t_start_1 = perf_counter()
+    C_list = pyMatmul.Matmul(A_list,B_list)
+    t_stop_1 = perf_counter()
+    time = (t_stop_1 - t_start_1)*1000
+    t[i-5] = time
+    
+    #t[i] = time
+    
+    t_start_2 = perf_counter()
+    C=A@B
+    t_stop_2 = perf_counter()
+    t_1[i-5] = (t_stop_2 - t_start_2)*1000
+    
+
+
+n=2**8
+size[3]=n
+A_list = generateRandomMatrix(n)
+B_list = generateRandomMatrix(n)
+    
+A=np.zeros([n,n])
+B=np.zeros([n,n])
+for i in range(n):
+    for j in range(n):
+        A[i][j]=A_list[i][j]
+        B[i][j]=B_list[i][j]
+    
+t_start_1 = perf_counter()
+C_list = pyMatmul.Matmul(A_list,B_list)
+t_stop_1 = perf_counter()
+t[3] = (t_stop_1-t_start_1)*1000
+
+
+t_start_2 = perf_counter()
+C=A@B
+t_stop_2 = perf_counter()
+t_1[3] = (t_stop_2-t_start_2)*1000
+print(str(C_list[0][0]) + "\n" + str((t_stop_1-t_start_1)*1000) + "\n" + str(C[0][0]) + "\n" + str((t_stop_2 - t_start_2)*1000))
+
+
+x = np.log2(size)
+y = np.log10(t)
+y_1 = np.log10(t_1)
+
+if(plotbool):
+    plt.plot(x,y)
+    plt.plot(x,y_1)
+    plt.show()
+
