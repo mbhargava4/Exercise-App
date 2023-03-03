6ca7374986c8a8c944fcf31aa46ac8d21a2d7714
mbhargava4 <118226828+mbhargava4@users.noreply.github.com>
2023-03-02 20:58:11 -0600

    h

--- A05/fNorm.py
+++ A05/fNorm.py
@@ -6,10 +6,12 @@ file_name = sys.argv[1]
 
 with open(file_name + ".csv") as file:
     csvreader = csv.reader(file, delimiter = "\t")
-    
-
+    x = 0
     for matrix in csvreader:
-        m = [list(0 for i in range(matrix)) for j in range(matrix)]
+        x = len(matrix)
+ 
+    m = [list(0 for i in range(x)) for j in range(x)]
+    for matrix in csvreader:
         for i in range(matrix):
             for j in range(matrix):
                 m[i][j] = float(matrix[i][j])
