fd06f3db2c2a7b893a83b078ed6ce9964c601eb3
mbhargava4 <118226828+mbhargava4@users.noreply.github.com>
2023-03-02 20:55:10 -0600

    h

--- A05/fNorm.py
+++ A05/fNorm.py
@@ -6,11 +6,14 @@ file_name = sys.argv[1]
 
 with open(file_name + ".csv") as file:
     csvreader = csv.reader(file, delimiter = "\t")
+    
 
     for matrix in csvreader:
+        m = [list(0 for i in range(matrix)) for j in range(matrix)]
         for i in range(matrix):
-            matrix[i] = float(matrix[i])
-        arr = np.array(matrix)
+            for j in range(matrix):
+                m[i][j] = float(matrix[i][j])
+        arr = np.array(m)
 
 f_norm = np.linalg.norm(arr, 'fro')
 print(f_norm)
