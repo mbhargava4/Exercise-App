bb8fdb1477352098e309583e3be59c92626c524b
mbhargava4 <118226828+mbhargava4@users.noreply.github.com>
2023-03-16 01:12:31 -0500

    task 2 random

---
+++ A06/task2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structs.h"
+
+int main() {
+    // Print size of struct A
+    printf("Size of struct A: %zu bytes\n", sizeof(struct A));
+    return 0;
+}
