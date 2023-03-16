340f7028c7853ecfcd86a42bc2444d8085b4c16d
mbhargava4 <118226828+mbhargava4@users.noreply.github.com>
2023-03-16 10:18:49 -0500

    task 2 hopefully done

--- A06/task2.c
+++ A06/task2.c
@@ -3,8 +3,27 @@
 #include "structs.h"
 
 int main() {
-    // Print size of struct A
-    printf("Size of struct A: %zu bytes\n", sizeof(struct A));
-    printf("Size of struct B: %zu bytes\n", sizeof(struct B));
+    //Part A
+    printf("Size of struct A: %zu bytes\n", sizeof(struct A)); // Print size of struct A
+    printf("Size of struct B: %zu bytes\n", sizeof(struct B)); //Print size of struct B
+
+    //Part B
+    //Part C
+    //Part D
+    //Part E
+    //Part F
+
+    struct A* a = (struct A*)malloc(sizeof(struct A));
+    a->i = 10;
+    a->c = 'a';
+    a->d = 3.14;
+
+    printf("Field 1: %d\n", a->i);
+    printf("Field 2: %c\n", a->c);
+    printf("Field 3: %f\n", a->d);
+
+    free(a);
+
+
     return 0;
 }
