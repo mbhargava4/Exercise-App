10f19716d04da30e56d6d69320540ad837dc579b
mbhargava4 <118226828+mbhargava4@users.noreply.github.com>
2023-03-16 01:14:10 -0500

    task 2-size of struct A and B

--- A06/task2.c
+++ A06/task2.c
@@ -5,5 +5,6 @@
 int main() {
     // Print size of struct A
     printf("Size of struct A: %zu bytes\n", sizeof(struct A));
+    printf("Size of struct B: %zu bytes\n", sizeof(struct B));
     return 0;
 }
