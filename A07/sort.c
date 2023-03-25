48a747eaabdb17f00b57d5b0b5469bab73de016c
mbhargava4 <118226828+mbhargava4@users.noreply.github.com>
2023-03-24 19:47:36 -0500

    P1 hopefully complete

---
+++ A07/sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+static void bubble_sort(int* A, size_t n)
+{
+    for (size_t i = 0; i < n - 1; ++i) {
+        for (size_t j = 0; j < n - i - 1; ++j) {
+            if (A[j] > A[j + 1]) {
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
+        }
+    }
+}
