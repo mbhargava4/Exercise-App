88d1ae28c558d8b8631db52b254bfc54bc774d7d
mbhargava4 <118226828+mbhargava4@users.noreply.github.com>
2023-05-07 12:41:27 -0500

    Done Commenting on Files

--- FinalProject459/pushuplearner.py
+++ FinalProject459/pushuplearner.py
@@ -1,16 +1,15 @@
-# This code begins by importing various libraries such as tryout2module, cv2, numpy, pathlib, mediapipe, and time. It then defines a 
+# This code begins by importing various libraries such as PoseDetector , cv2, numpy, pathlib, mediapipe, and time. It then defines a 
 # path variable that contains the path to a video file named "pushupdemo3.mp4". This video file is read using cv2.VideoCapture() and is 
-# assigned to a variable called cap1.
-
-Next, the code initializes three empty lists named angle1listarm, anglelistback, and anglelistneck, as well as some other variables such as dir, reps, and ptime.
-
-The code then uses an instance of tryout2module.PoseDetector() to detect poses in each frame of the video and obtain the angles between various joints. These angles are appended to the relevant lists.
-
-The code also calculates the percentage of the push-up completed by interpolating the angle between the arms. It then checks if a push-up has been completed and increments the reps variable accordingly.
-
-The code finally displays the number of reps completed using cv2.putText(), and waits for the "e" key to be pressed to exit the program. Once all frames of the video have been processed, the maximum and minimum angles for each of the arm, back, and neck joints are calculated.
-
-Overall, this code seems to be detecting push-up exercises in a video file and counting the number of repetitions completed. It also calculates the maximum and minimum angles for the arm, back, and neck joints. However, it is difficult to comment further without knowing the implementation of tryout2module.PoseDetector().
+# assigned to a variable called cap1. Next, the code initializes three empty lists named angle1listarm, anglelistback, and anglelistneck,
+# as well as some other variables such as dir, reps, and ptime. The code then uses an instance of PoseDetector .PoseDetector() to detect
+# poses in each frame of the video and obtain the angles between various joints. These angles are appended to the relevant lists. The 
+# code also calculates the percentage of the push-up completed by interpolating the angle between the arms. It then checks if a push-up 
+# has been completed and increments the reps variable accordingly. The code finally displays the number of reps completed using 
+# cv2.putText(), and waits for the "e" key to be pressed to exit the program. Once all frames of the video have been processed, the 
+# maximum and minimum angles for each of the arm, back, and neck joints are calculated. Overall, this code seems to be detecting 
+# push-up exercises in a video file and counting the number of repetitions completed. It also calculates the maximum and minimum 
+# angles for the arm, back, and neck joints. However, it is difficult to comment further without knowing the implementation of 
+#PoseDetector .PoseDetector().
 import PostureDetectorModule
 import cv2
 import numpy as np
