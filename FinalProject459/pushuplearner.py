2fdba42e088e9c616c94a68b0083c5f9e11a99d6
mbhargava4 <118226828+mbhargava4@users.noreply.github.com>
2023-05-07 08:39:11 -0500

    The Program can now count reps

--- FinalProject459/pushuplearner.py
+++ FinalProject459/pushuplearner.py
@@ -4,24 +4,42 @@ import numpy as np
 import pathlib
 import mediapipe as mp
 import math
+import time
 
-p1 = pathlib.PureWindowsPath(r'C:\Users\manan\PycharmProjects\FinalProject459\VideoSample\pushupdemo3.mp4')
+p1 = pathlib.PureWindowsPath(r'C:\Users\manan\PycharmProjects\FinalProject459\VideoSample\pushupdemo5.mp4')
 cap1 = cv2.VideoCapture(p1.as_posix())
-angle1left = 0
-angle1listleft = []
+angle1arm = 0
+angle1listarm = []
+dir = 0
+reps = 0
+ptime = time.perf_counter()
+repCounted = False
 detector = tryout2module.PoseDetector()
 
 for frameIndex in range(int(cap1.get(cv2.CAP_PROP_FRAME_COUNT))):
     success, frame = cap1.read()
     frame = detector.findPose(frame, False)
     lmlist = detector.getPosition(frame, False)
-    if frameIndex == 0:
-        angle1listleft.append(0)
 
     if len(lmlist) != 0:
-        detector.getAngle(frame, 11, 13, 15, True)
-        angleleft = detector.getAngle(frame, 11, 13, 15, True)
-        angle1listleft.append(angleleft)
+        detector.getAngle(frame, 11, 13, 15, False)
+        anglearm = detector.getAngle(frame, 11, 13, 15, False)
+        angle1listarm.append(anglearm)
+        percentdone = np.interp(anglearm, (20,80), (0,100))
+        ctime = time.perf_counter()
+
+        if percentdone==100:
+            if dir==0:
+                reps+=1
+                dir=1
+                if((ctime-ptime)<1 ):
+                    reps -= 1
+        if percentdone == 0:
+            if dir == 1:
+                dir=0
+
+
+    cv2.putText(frame, str(int(reps)), (50,100), cv2.FONT_HERSHEY_COMPLEX,2,(0, 0, 255), 5)
     cv2.imshow('Push Up demo', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('e'):
@@ -30,8 +48,5 @@ for frameIndex in range(int(cap1.get(cv2.CAP_PROP_FRAME_COUNT))):
 cap1.release()
 cv2.destroyAllWindows()
 
-
-max_angle1left = max(angle1listleft)
-print(max_angle1left)
-min_angle1left = min(angle1listleft)
-print(min_angle1left)
+max_angle1left = max(angle1listarm)
+min_angle1left = min(angle1listarm)
