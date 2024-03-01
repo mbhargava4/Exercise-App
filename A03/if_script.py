cfd8800a8b68edaa499085ecf92606833eab176e
mbhargava4 <118226828+mbhargava4@users.noreply.github.com>
2024-03-01 03:55:47 -0600

    Removed files

--- A03/if_script.py
+++
@@ -1,6 +0,0 @@
-n = input("Enter a value ")
-if(int((int(n))/10)==0):
-    print("It is a one digit number")
-else:
-    print("iT is a two digit number")
-
--- Practice.py
+++
@@ -1,5 +0,0 @@
-fifi = ('one','two')
-mimi = fifi
-mimi[1] = 'three'
-print(fifi)
-print(mimi)
